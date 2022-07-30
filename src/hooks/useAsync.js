74265a86f1bf94953dc02800aabb38242d176a1d
Henrique Nunes <henri.arantes1@gmail.com>
2022-07-30 00:05:40 -0300

    adding useAsync

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,43 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: 'idle',
+  });
+
+  const run = useCallback(() => {
+    setState({
+      result: null,
+      error: null,
+      status: 'pending',
+    });
+
+    return asyncFunction()
+      .then((response) => {
+        setState({
+          result: response,
+          error: null,
+          status: 'settled',
+        });
+      })
+      .catch((error) => {
+        setState({
+          result: null,
+          error: error,
+          status: 'error',
+        });
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    shouldRun && run();
+  }, [run, shouldRun]);
+
+  const { result, error, status } = state;
+
+  return [run, result, error, status];
+};
+
+export default useAsync;
